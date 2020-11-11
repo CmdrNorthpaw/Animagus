73dd3abb3babf423ef3085fb9c65f613475842a5
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-11 20:19:55 +0000

    Some more jungle-type animals

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Catalysts.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Catalysts.kt
@@ -17,6 +17,8 @@ enum class Catalysts(type: EntityType<out LivingEntity>, vararg items: Item) {
     CHICKEN(EntityType.CHICKEN, Items.EGG),
     COW(EntityType.COW, Items.LEATHER),
     PIG(EntityType.PIG, Items.APPLE),
-    SHEEP(EntityType.SHEEP, Items.WHITE_WOOL, Items.BROWN_WOOL, Items.BLUE_WOOL, Items.BLUE_WOOL, Items.LIGHT_BLUE_WOOL, Items.GRAY_WOOL, Items.LIGHT_GRAY_WOOL, Items.BLACK_WOOL, Items.CYAN_WOOL, Items.GREEN_WOOL, Items.ORANGE_WOOL, Items.LIME_WOOL, Items.RED_WOOL, Items.YELLOW_WOOL, Items.PURPLE_WOOL, Items.MAGENTA_WOOL, Items.PINK_WOOL)
+    SHEEP(EntityType.SHEEP, Items.WHITE_WOOL, Items.BROWN_WOOL, Items.BLUE_WOOL, Items.BLUE_WOOL, Items.LIGHT_BLUE_WOOL, Items.GRAY_WOOL, Items.LIGHT_GRAY_WOOL, Items.BLACK_WOOL, Items.CYAN_WOOL, Items.GREEN_WOOL, Items.ORANGE_WOOL, Items.LIME_WOOL, Items.RED_WOOL, Items.YELLOW_WOOL, Items.PURPLE_WOOL, Items.MAGENTA_WOOL, Items.PINK_WOOL),
 
+    PARROT(EntityType.PARROT, Items.COOKIE),
+    PANDA(EntityType.PANDA, Items.BAMBOO)
 }
