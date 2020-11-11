d574eebdedbe73f7bc4d6bf96bf3dd6a66585b80
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-11 20:15:36 +0000

    Make Items a vararg so we can have more than one catalyst per entity

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Catalysts.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Catalysts.kt
@@ -7,14 +7,16 @@ import net.minecraft.item.Item
 import net.minecraft.item.Items
 import uk.cmdrnorthpaw.animagus.items.AnimagusItems
 
-enum class Catalysts(item: Item, type: EntityType<out LivingEntity>) {
-    CAT(AnimagusItems.CAT_HAIR, EntityType.CAT),
-    DOG(Items.BONE, EntityType.WOLF),
+enum class Catalysts(type: EntityType<out LivingEntity>, vararg items: Item) {
+    CAT(EntityType.CAT, AnimagusItems.CAT_HAIR),
+    DOG(EntityType.WOLF, Items.BONE),
 
-    SALMON(Items.SALMON, EntityType.SALMON),
-    COD(Items.COD, EntityType.COD),
+    SALMON(EntityType.SALMON, Items.SALMON),
+    COD(EntityType.COD, Items.COD),
 
-    CHICKEN(Items.EGG, EntityType.CHICKEN),
-    COW(Items.LEATHER, EntityType.COW),
-    PIG(Items.APPLE, EntityType.PIG),
+    CHICKEN(EntityType.CHICKEN, Items.EGG),
+    COW(EntityType.COW, Items.LEATHER),
+    PIG(EntityType.PIG, Items.APPLE),
+    SHEEP(EntityType.SHEEP, Items.WHITE_WOOL, Items.BROWN_WOOL, Items.BLUE_WOOL, Items.BLUE_WOOL, Items.LIGHT_BLUE_WOOL, Items.GRAY_WOOL, Items.LIGHT_GRAY_WOOL, Items.BLACK_WOOL, Items.CYAN_WOOL, Items.GREEN_WOOL, Items.ORANGE_WOOL, Items.LIME_WOOL, Items.RED_WOOL, Items.YELLOW_WOOL, Items.PURPLE_WOOL, Items.MAGENTA_WOOL, Items.PINK_WOOL)
+
 }
