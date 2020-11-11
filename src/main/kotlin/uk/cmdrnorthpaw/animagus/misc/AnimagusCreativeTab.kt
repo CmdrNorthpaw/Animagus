5dee6bf1ad8a8e57682594c375a61f1cbf2bb6f5
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-11 21:02:46 +0000

    New ItemGroup for our items

---
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/AnimagusCreativeTab.kt
@@ -0,0 +1,9 @@
+package uk.cmdrnorthpaw.animagus.misc
+
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import uk.cmdrnorthpaw.animagus.items.AnimagusItems
+
+object AnimagusCreativeTab: ItemGroup("Animagus") {
+    override fun createIcon(): ItemStack = ItemStack(AnimagusItems.MANDRAKE_LEAF, 1)
+}
