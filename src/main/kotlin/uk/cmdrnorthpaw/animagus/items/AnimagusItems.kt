4765611d51d33cc7614e5eaf273d7f6f85e63045
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-11 15:55:11 +0000

    Add cat hair to AnimagusItems.kt

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/items/AnimagusItems.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/items/AnimagusItems.kt
@@ -3,7 +3,10 @@ package uk.cmdrnorthpaw.animagus.items
 import net.minecraft.item.Food
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
+import uk.cmdrnorthpaw.animagus.items.catalysts.CatHair
 
 object AnimagusItems {
     val MANDRAKE_LEAF = MandrakeLeaf()
+
+    val CAT_HAIR = CatHair()
 }
