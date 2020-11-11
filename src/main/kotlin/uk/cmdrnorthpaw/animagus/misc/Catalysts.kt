8c973a628f098755c72f62dbe56b50262c481856
CmdrNorthpaw <thetuxkid@gmail.com>
2020-11-11 20:04:59 +0000

    Add some more entities to Catalysts

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Catalysts.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Catalysts.kt
@@ -1,12 +1,20 @@
 package uk.cmdrnorthpaw.animagus.misc
 
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.passive.AnimalEntity
 import net.minecraft.item.Item
 import net.minecraft.item.Items
 import uk.cmdrnorthpaw.animagus.items.AnimagusItems
 
-enum class Catalysts(item: Item, type: EntityType<out AnimalEntity>) {
+enum class Catalysts(item: Item, type: EntityType<out LivingEntity>) {
     CAT(AnimagusItems.CAT_HAIR, EntityType.CAT),
-    DOG(Items.BONE, EntityType.WOLF)
+    DOG(Items.BONE, EntityType.WOLF),
+
+    SALMON(Items.SALMON, EntityType.SALMON),
+    COD(Items.COD, EntityType.COD),
+
+    CHICKEN(Items.EGG, EntityType.CHICKEN),
+    COW(Items.LEATHER, EntityType.COW),
+    PIG(Items.APPLE, EntityType.PIG),
 }
