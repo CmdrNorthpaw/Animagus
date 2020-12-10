d719711afe393ce94ee98590b2d033efd4938141
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-10 08:43:50 +0000

    Add tag providers to Data Generators

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/data/DataGenerators.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/data/DataGenerators.kt
@@ -1,10 +1,12 @@
 package uk.cmdrnorthpaw.animagus.data
 
 import net.minecraft.data.DataGenerator
+import net.minecraftforge.common.data.ExistingFileHelper
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent
 import uk.cmdrnorthpaw.animagus.Animagus
+import uk.cmdrnorthpaw.animagus.data.tags.ItemTagProvider
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = Animagus.MODID)
 object DataGenerators {
@@ -13,14 +15,15 @@ object DataGenerators {
 
     }
 
-    private fun registerServer(generator: DataGenerator) {
+    private fun registerServer(generator: DataGenerator, helper: ExistingFileHelper) {
         generator.addProvider(AnimagusRecipeHandler(generator))
+        generator.addProvider(ItemTagProvider(generator, helper))
     }
 
     @JvmStatic
     @SubscribeEvent
     fun gatherData(event: GatherDataEvent) {
-        if (event.includeServer()) registerServer(event.generator)
+        if (event.includeServer()) registerServer(event.generator, event.existingFileHelper)
         if (event.includeClient()) registerClient(event.generator)
     }
 }
