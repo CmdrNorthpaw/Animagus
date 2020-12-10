86ec0b69954e4414e55408dbc49b2289e07c2277
CmdrNorthpaw <thetuxkid@gmail.com>
2020-12-10 09:03:22 +0000

    ItemModelProvider!

---
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/data/models/ModelProviders.kt
@@ -0,0 +1,18 @@
+package uk.cmdrnorthpaw.animagus.data.models
+
+import net.minecraft.data.DataGenerator
+import net.minecraftforge.client.model.generators.ItemModelBuilder
+import net.minecraftforge.client.model.generators.ItemModelProvider
+import net.minecraftforge.common.data.ExistingFileHelper
+import uk.cmdrnorthpaw.animagus.Animagus
+
+class AnimagusItemModelProvider(generator: DataGenerator, helper: ExistingFileHelper) : ItemModelProvider(generator, Animagus.MODID, helper) {
+    private val itemGenerated = getExistingFile(mcLoc("item/generated"))
+
+    override fun registerModels() {
+        itemModelBuilder("crystal_phial")
+        itemModelBuilder("dew_phial")
+    }
+
+    private fun itemModelBuilder(name: String): ItemModelBuilder = getBuilder(name).parent(itemGenerated).texture("layer0", name)
+}
