31c9bcd6a99370dd7c96c71788eeddcd2ac853d8
CmdrNorthpaw <thetuxkid@gmail.com>
2021-03-01 15:13:22 +0000

    Restructured Capabilities.kt to act as a list-like object

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Capabilities.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/misc/Capabilities.kt
@@ -3,9 +3,13 @@ package uk.cmdrnorthpaw.animagus.misc
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.capabilities.CapabilityInject
 import uk.cmdrnorthpaw.animagus.model.IAnimagusCapability
-import uk.cmdrnorthpaw.animagus.player.AnimagusCapability
+import uk.cmdrnorthpaw.animagus.capability.AnimagusCapability
+import uk.cmdrnorthpaw.animagus.capability.CatalystCapability
 
 object Capabilities {
     @CapabilityInject(IAnimagusCapability::class)
     lateinit var ANIMAGUS: Capability<AnimagusCapability>
+
+    @CapabilityInject(IAnimagusCapability::class)
+    lateinit var CATALYST: Capability<CatalystCapability>
 }
