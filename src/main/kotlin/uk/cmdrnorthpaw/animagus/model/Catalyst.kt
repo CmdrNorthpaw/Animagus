0d61e6c431cc5150a8824b687474b09d1abb43c4
CmdrNorthpaw <thetuxkid@gmail.com>
2021-03-01 11:31:41 +0000

    Make Catalyst use EntityTypes instead of Entities and allow you to convert lists of Items

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/model/Catalyst.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/model/Catalyst.kt
@@ -1,23 +1,31 @@
 package uk.cmdrnorthpaw.animagus.model
 
+import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.MobEntity
 import net.minecraft.item.Item
 import net.minecraft.tags.ITag
 
 interface Catalyst {
-    val entity: LivingEntity
+    val entity: EntityType<out MobEntity>
     val items: Array<Item>
 
     companion object {
         @JvmStatic
-        fun Item.asCatalystFor(entity: LivingEntity): Catalyst = object: Catalyst {
-            override val entity = entity
+        fun Item.asCatalystFor(entity: MobEntity): Catalyst = object: Catalyst {
+            override val entity = entity.type as EntityType<out MobEntity>
             override val items = arrayOf(this@asCatalystFor)
         }
 
         @JvmStatic
-        fun ITag.INamedTag<Item>.asCatalystFor(entity: LivingEntity) = object : Catalyst {
-            override val entity = entity
+        fun Array<Item>.asCatalystFor(entity: MobEntity) = object : Catalyst {
+            override val entity: EntityType<out MobEntity> = entity.type as EntityType<out MobEntity>
+            override val items = this@asCatalystFor
+        }
+
+        @JvmStatic
+        fun ITag.INamedTag<Item>.asCatalystFor(entity: MobEntity) = object : Catalyst {
+            override val entity = entity.type as EntityType<out MobEntity>
             override val items: Array<Item> = this@asCatalystFor.allElements.toTypedArray()
         }
     }
