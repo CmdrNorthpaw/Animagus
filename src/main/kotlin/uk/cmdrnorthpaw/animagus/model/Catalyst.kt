4960a7478ecc27dc1b994e4370de171277500c6f
CmdrNorthpaw <thetuxkid@gmail.com>
2021-03-02 21:16:04 +0000

    Make Catalyst static methods use EntityTypes

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/model/Catalyst.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/model/Catalyst.kt
@@ -12,20 +12,20 @@ interface Catalyst {
 
     companion object {
         @JvmStatic
-        fun Item.asCatalystFor(entity: MobEntity): Catalyst = object: Catalyst {
-            override val entity = entity.type as EntityType<out MobEntity>
+        fun Item.asCatalystFor(entity: EntityType<out MobEntity>): Catalyst = object: Catalyst {
+            override val entity = entity
             override val items = arrayOf(this@asCatalystFor)
         }
 
         @JvmStatic
-        fun Array<Item>.asCatalystFor(entity: MobEntity) = object : Catalyst {
-            override val entity: EntityType<out MobEntity> = entity.type as EntityType<out MobEntity>
+        fun Array<Item>.asCatalystFor(entity: EntityType<out MobEntity>) = object : Catalyst {
+            override val entity: EntityType<out MobEntity> = entity
             override val items = this@asCatalystFor
         }
 
         @JvmStatic
-        fun ITag.INamedTag<Item>.asCatalystFor(entity: MobEntity) = object : Catalyst {
-            override val entity = entity.type as EntityType<out MobEntity>
+        fun ITag.INamedTag<Item>.asCatalystFor(entity: EntityType<out MobEntity>) = object : Catalyst {
+            override val entity = entity
             override val items: Array<Item> = this@asCatalystFor.allElements.toTypedArray()
         }
     }
