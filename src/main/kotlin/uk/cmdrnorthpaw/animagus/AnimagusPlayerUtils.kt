f37af381d91dfdb2c8bc6dcf43f1d4decfd77f23
CmdrNorthpaw <thetuxkid@gmail.com>
2021-03-02 16:48:58 +0000

    Abstract away the morphing ability (maybe this is what abstraction means. Who's to say?

--- src/main/kotlin/uk/cmdrnorthpaw/animagus/AnimagusPlayerUtils.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/animagus/AnimagusPlayerUtils.kt
@@ -8,24 +8,33 @@ import net.minecraft.network.IPacket
 import net.minecraft.server.management.PlayerList
 import net.minecraft.world.World
 import com.mojang.datafixers.util.Pair
+import net.minecraft.entity.EntityType
 import net.minecraft.entity.MobEntity
 import net.minecraft.network.play.server.*
 import net.minecraft.util.*
+import uk.cmdrnorthpaw.animagus.AnimagusPlayerUtils.toAnimagus
 import uk.cmdrnorthpaw.animagus.misc.Capabilities
 import java.util.*
 import java.util.stream.Collectors
 
 object AnimagusPlayerUtils {
-    fun PlayerEntity.toAnimagus() = this.getCapability(Capabilities.ANIMAGUS).ifPresent { capability ->
-        if (capability.animagusForm == null || !capability.isAnimagus) return@ifPresent
+    fun PlayerEntity.toAnimagus() {
+        getCapability(Capabilities.ANIMAGUS).ifPresent { capability ->
+            if (capability.isInAnimagusForm || !capability.isAnimagus) return@ifPresent
+             val form = capability.animagusForm
 
+            form?.let { morph(form) }
+        }
+    }
+
+    private fun PlayerEntity.morph(type: EntityType<*>? = null) {
         val worldKey = this.entityWorld.dimensionKey
         val playerList = this.server?.playerList
-        val disguiseEntity = capability.animagusForm?.create(world)
+        val disguiseEntity = if (type == null) this else type.create(world)
 
-        if (disguiseEntity !is MobEntity) {
+        if (disguiseEntity !is MobEntity && disguiseEntity !is PlayerEntity) {
             Animagus.logger.error("Player ${name.unformattedComponentText} attempted to morph into not a mob! This is *not* allowed")
-            return@ifPresent
+            return
         }
 
         playerList?.sendPacketToDimension(SDestroyEntitiesPacket(entityId), worldKey)
@@ -35,8 +44,14 @@ object AnimagusPlayerUtils {
         playerList?.sendPacketToDimension(SEntityHeadLookPacket(disguiseEntity, (getRotationYawHead() * 256F / 360F).toInt().toByte()), worldKey)
 
         this.world.playSound(null, posX, posY, posZ, SoundEvents.ENTITY_GUARDIAN_AMBIENT, SoundCategory.PLAYERS, 1F, 2F)
+    }
 
-        capability.isInAnimagusForm = true
+    fun PlayerEntity.toPlayer() {
+        getCapability(Capabilities.ANIMAGUS).ifPresent { capability ->
+            if (!capability.isInAnimagusForm) return@ifPresent
+
+            morph()
+        }
     }
 
     private fun getEquipment(entity: LivingEntity): List<Pair<EquipmentSlotType, ItemStack>> {
